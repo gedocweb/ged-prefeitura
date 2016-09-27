cef94ec698c81f40f77ac9011cab6158db5dbfdb
Pedro Augusto <pedroaugusto.gti@gmail.com>
2016-09-27 15:30:52 -0300

    Customização para utilização da versão PREFEITURA e PRIVADO no mesmo projeto para evitar duplicação de solução de bugs.

---
+++ dml.sql
@@ -0,0 +1,54 @@
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+1,'Ofício',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+2,'Ata',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+3,'Atestado',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+4,'Aviso',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+5,'Carta',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+6,'Certidão',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+7,'Circular',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+8,'Decreto',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+9,'Decreto-Lei',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+10,'Despachos',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+11,'Edital',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+12,'Exposição de Motivos',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+13,'Lei',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+14,'Memorando',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+15,'Mensagem',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+16,'Notas',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+17,'Ordem de Serviço',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+18,'Portaria',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+19,'Regimento',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+20,'Regulamento',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+21,'Relatório',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+22,'Requerimento',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+23,'Escritura',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+24,'Contrato',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+25,'Certificado',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+26,'Declaração',0);
+INSERT INTO public.tb_tipo_doc (id_tipo_doc,descricao,situacao) VALUES (
+27,'Histórico',0);
